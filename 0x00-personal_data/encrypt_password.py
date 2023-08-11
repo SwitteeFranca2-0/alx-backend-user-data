38164cf6dc5bd5b278728086a66d5a8a0dad16a1
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-08-11 18:06:26 +0100

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,11 +4,13 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
+    """encrpt password"""
     pwd = bytes(password, 'utf-8')
     return bcrypt.hashpw(pwd, bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
+    """check password against encrypted password"""
     if bcrypt.checkpw(bytes(password, 'utf-8'), hashed_password):
         return True
     return False
