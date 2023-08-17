28cd2886282dface99581255eaadbe43f146bf5e
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-08-17 13:32:14 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -74,7 +74,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except Exception:
             raise ValueError
-        token = str(uuid.uuid4())
+        token = str(uuid4())
         self._db.update_user(user.id, reset_token=token)
         return token
 
