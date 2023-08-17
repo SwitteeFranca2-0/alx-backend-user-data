4bbdef57f1b417ffe4d507059c2056be3aa1b181
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-08-17 07:52:32 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -26,7 +26,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
-            return self._db.add_user(email, _hash_password(password)) 
+            return self._db.add_user(email, _hash_password(password))
         raise ValueError('User {} already exists'.format(email))
 
     def valid_login(self, email: str, password: str) -> bool:
